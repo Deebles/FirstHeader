7fb97cc44f0c220afbfe5b90829cc334c055ea7f
Daniel <danielbrown@madbookpro.local>
2015-10-02 20:29:36 +0100

    Initial Commit

---
+++ FirstHeader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstHeader
+//
+//  Created by Daniel on 02/10/2015.
+//  Copyright © 2015 Daniel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
